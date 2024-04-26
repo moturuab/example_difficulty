0bd4b8ba916ebfe48e12988848b2bfd371d9c3bb
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-04-25 20:33:58 -0400

    caltech256 categories

--- caltech256.py
+++ caltech256.py
@@ -78,7 +78,7 @@ for word1 in items.keys():
             syns2 = wordnet.synsets(word2)
             if syns1 and syns2:
                 s = syns1[0].wup_similarity(syns2[0])
-                if s > 0.7:
+                if s > 0.8:
                     if items[w1] not in sims:
                         sims[items[w1]] = [items[w2]]
                     else:
