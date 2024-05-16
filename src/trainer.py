6a49de7a61298c1dde3703e20af25c62b88c41c1
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-05-15 22:46:30 -0400

    try aum reweighting

--- src/trainer.py
+++ src/trainer.py
@@ -176,10 +176,11 @@ class PyTorchTrainer:
                 loss = self.criterion(outputs, observed_label)
                 if epoch > 0:
                     sample_weight = self.aum.scores
+                    sample_weight = (sample_weight-torch.min(sample_weight))/(torch.max(sample_weight)-torch.min(sample_weight))
                     print(len(sample_weight))
                     print(max(sample_weight))
                     print(min(sample_weight))
-                    loss =(loss * sample_weight / sample_weight.sum()).sum()
+                    loss = torch.mul(loss, sample_weight)
 
                 loss.backward()
                 self.optimizer.step()
@@ -219,9 +220,9 @@ class PyTorchTrainer:
                 print("vog")
                 self.vog.updates(net=self.model, device=self.device)
 
-            if self.loss is not None:
-                print("loss")
-                self.loss.updates(logits=logits, targets=targets)
+            #if self.loss is not None:
+            #    print("loss")
+            #    self.loss.updates(logits=logits, targets=targets)
 
         # These HCMs are applied after training
         if self.prototypicality is not None:
