45326519d99f22650fe517efd35b203974abe6b9
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-12 02:13:54 -0400

    fix loss imports

--- src/trainer.py
+++ src/trainer.py
@@ -193,6 +193,8 @@ class PyTorchTrainer:
                 print(3)
                 print(self.alpha.grad)
                 self.alpha.grad.zero_()
+                print(3.5)
+                print(self.alpha.grad)
 
                 for j, val_data in enumerate(val_dataloader):
                     val_inputs, val_true_label, val_observed_label, val_indices = val_data
