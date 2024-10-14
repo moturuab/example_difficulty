ebe39e9c122faf2a539a8701e4c9b41c64e89a04
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-14 05:12:42 -0400

    fix loss

--- src/losses.py
+++ src/losses.py
@@ -21,6 +21,9 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
         softmax_outputs = self.softmax(outputs)
         correct_outputs = softmax_outputs.gather(1, torch.argmax(encoded_targets, dim=1).unsqueeze(1)).squeeze(1)
         max_outputs = softmax_outputs.gather(1, torch.argmax(softmax_outputs, dim=1).unsqueeze(1)).squeeze(1)
+        print(torch.min(self.alpha*correct_outputs - max_outputs))
+        print(torch.mean(self.alpha*correct_outputs - max_outputs))
+        print(torch.max(self.alpha*correct_outputs - max_outputs))
         weights = 1/1+(torch.exp(-(self.alpha*correct_outputs - max_outputs)))
         return weights
 
