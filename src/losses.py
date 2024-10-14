3953c5a8e70551f3f047787e8a7bed46195867f0
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-13 23:42:20 -0400

    fix loss

--- src/losses.py
+++ src/losses.py
@@ -46,7 +46,7 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
                     print(torch.min(weights[weights>0]))
                     print(torch.mean(weights[weights>0]))
                     print(torch.max(weights[weights>0]))
-                    weights[weights<0] = torch.min(weights[weights>0]) + torch.mean(weights[weights>0])*(weights[weights<0] - torch.min(weights[weights<0]))/(torch.max(weights[weights<0]) - torch.min(weights[weights<0]))
+                    #weights[weights<0] = torch.min(weights[weights>0]) + torch.mean(weights[weights>0])*(weights[weights<0] - torch.min(weights[weights<0]))/(torch.max(weights[weights<0]) - torch.min(weights[weights<0]))
                 else:
                     weights[weights<0] = - torch.mean(weights[weights>0]) * (weights[weights<0] - torch.min(weights[weights<0]))/(torch.max(weights[weights<0]) - torch.min(weights[weights<0]))
             weighted_loss = weights * loss
