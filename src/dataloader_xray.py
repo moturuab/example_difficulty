e17a76baa52d98432eb4cd1b7dce009d85844275
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-16 01:19:41 -0400

    added nih similarities

--- src/dataloader_xray.py
+++ src/dataloader_xray.py
@@ -30,7 +30,7 @@ class XraySubsetDataset(Dataset):
     def __getitem__(self, idx):
         image = self.dataset[idx]["img"]
         target = self.labels[idx]
-        return image, target.tolist()
+        return image, target
 
     def __len__(self):
         return len(self.labels)
