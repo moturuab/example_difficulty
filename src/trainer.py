ca6edbd3a98a8d5e7426fc977635306817bbf624
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-16 02:44:25 -0400

    added nih similarities

--- src/trainer.py
+++ src/trainer.py
@@ -29,11 +29,10 @@ def accuracy(output, target, topk=(1,5,)):
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
-    print(correct)
 
     res = []
     for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
+        correct_k = correct[:k].reshape(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
 
