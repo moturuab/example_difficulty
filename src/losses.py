b71d7e66af9957a866aee96de8ff8ae8425af62e
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-17 13:16:04 -0400

    try alpha and beta with powers

--- src/losses.py
+++ src/losses.py
@@ -25,9 +25,9 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
         correct_outputs = softmax_outputs.gather(1, torch.argmax(encoded_targets, dim=1).unsqueeze(1)).squeeze(1)
         max_outputs = softmax_outputs.gather(1, torch.argmax(softmax_outputs, dim=1).unsqueeze(1)).squeeze(1)
         if not m:
-            weights = self.sigmoid(self.alpha*correct_outputs - max_outputs+self.alpha)**self.alpha
+            weights = self.sigmoid(self.alpha*correct_outputs - max_outputs + self.alpha)**self.alpha
         else:
-            weights = self.sigmoid(-(self.beta*correct_outputs - max_outputs))**2
+            weights = self.sigmoid(-(self.beta*correct_outputs - max_outputs + self.alpha))**self.beta
         return weights
 
     def forward(self, outputs, targets, m=0):
