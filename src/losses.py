4c5e7b2ace399743ff444f13f45ccce8e82c4663
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-17 02:34:59 -0400

    try alpha and beta with powers

--- src/losses.py
+++ src/losses.py
@@ -27,7 +27,7 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
         if not m:
             weights = self.sigmoid(self.alpha*correct_outputs - max_outputs)**0.5
         else:
-            weights = self.sigmoid(-(self.beta*correct_outputs - max_outputs))**1
+            weights = self.sigmoid(-(self.beta*correct_outputs - max_outputs))**2
         return weights
 
     def forward(self, outputs, targets, m=0):
