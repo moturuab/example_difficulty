df30bc927aefffa96b98c97dc0d1b87535dd4b4e
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-18 13:10:55 -0400

    try alpha and beta with wd

--- src/losses.py
+++ src/losses.py
@@ -29,7 +29,7 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
             print(torch.min(self.alpha*correct_outputs - max_outputs))
             print(torch.max(self.alpha*correct_outputs - max_outputs))
             #weights = self.sigmoid(self.alpha*correct_outputs - max_outputs)
-            weights = self.alpha * self.sigmoid(self.alpha*correct_outputs - max_outputs)
+            weights = self.sigmoid(self.alpha*correct_outputs - max_outputs)
             print(torch.min(weights))
             print(torch.max(weights))
         else:
