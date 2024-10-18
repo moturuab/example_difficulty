6804a442546222c40cc046bd20850dde02076ecd
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-18 17:14:35 -0400

    try alpha and beta with wd

--- src/losses.py
+++ src/losses.py
@@ -29,9 +29,9 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
             print(torch.min(self.alpha*correct_outputs - max_outputs))
             print(torch.max(self.alpha*correct_outputs - max_outputs))
             #weights = self.sigmoid(self.alpha*correct_outputs - max_outputs)
-            weights = (self.sigmoid(self.alpha*correct_outputs - max_outputs) * torch.exp(-(-(self.beta*correct_outputs - max_outputs))**2/2))
+            weights = (self.sigmoid(self.alpha*correct_outputs - max_outputs) + torch.exp(-(-(self.beta*correct_outputs - max_outputs))**2/2))/2
             #weights = self.sigmoid(self.alpha*correct_outputs - max_outputs)**(1/self.alpha)
-            weights = torch.where(weights < 0.05, 0, weights)
+            weights = torch.where(weights < 0.1, 0, weights)
 
             print(torch.min(weights))
             print(torch.max(weights))
@@ -40,11 +40,11 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
             print(torch.min(-(self.beta*correct_outputs - max_outputs)))
             print(torch.max(-(self.beta*correct_outputs - max_outputs)))
             # weights = torch.exp(-(-(self.beta*correct_outputs - max_outputs))**2/2)
-            weights = (self.sigmoid(self.alpha*correct_outputs - max_outputs) * torch.exp(-(-(self.beta*correct_outputs - max_outputs))**2/2))
+            weights = (self.sigmoid(self.alpha*correct_outputs - max_outputs) + torch.exp(-(-(self.beta*correct_outputs - max_outputs))**2/2))/2
             #weights = torch.exp(-(-(self.beta*correct_outputs - max_outputs))**2/2)**0.5
             print(torch.min(weights))
             print(torch.max(weights))
-            weights = torch.where(weights < 0.05, 0, weights)
+            weights = torch.where(weights < 0.1, 0, weights)
             #print(torch.min(weights))
             #print(torch.max(weights))
         return weights
