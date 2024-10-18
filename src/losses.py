9307511cd002acccbf5dec22eabcfb5f129209e0
Your Name <abhishek.moturu@mail.utoronto.ca>
2024-10-18 04:26:04 -0400

    try alpha and beta with wd

--- src/losses.py
+++ src/losses.py
@@ -29,7 +29,7 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
             print(torch.min(self.alpha*correct_outputs - max_outputs))
             print(torch.max(self.alpha*correct_outputs - max_outputs))
             #weights = self.sigmoid(self.alpha*correct_outputs - max_outputs + self.alpha)**self.alpha
-            weights = self.sigmoid(self.alpha*correct_outputs - max_outputs+1/self.alpha) #**self.alpha
+            weights = self.sigmoid(self.alpha*correct_outputs - max_outputs + self.alpha**2) #**self.alpha
             print(torch.min(weights))
             print(torch.max(weights))
         else:
@@ -37,7 +37,7 @@ class WeightedCrossEntropyLoss(nn.CrossEntropyLoss):
             print(torch.min(-(self.beta*correct_outputs - max_outputs)))
             print(torch.max(-(self.beta*correct_outputs - max_outputs)))
             #weights = self.sigmoid(-(self.beta*correct_outputs - max_outputs) + self.beta)**self.beta
-            weights = self.sigmoid(-(self.beta*correct_outputs - max_outputs)-1/self.beta) #**self.beta
+            weights = self.sigmoid(-(self.beta*correct_outputs - max_outputs + self.beta**2)) #**self.beta
             print(torch.min(weights))
             print(torch.max(weights))
             #weights = torch.where(weights > torch.max(weights)-0.1, 0, weights)
